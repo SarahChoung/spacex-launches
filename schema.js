ca9fd0619c4f33c638bd27012bc7554dd1d86149
Sarah Choung <sarahjchoung@gmail.com>
2020-09-09 17:40:45 -0700

    rockets added to query

--- schema.js
+++ schema.js
@@ -54,6 +54,25 @@ const RootQuery = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/rockets")
+          .then((res) => res.data);
+      },
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
