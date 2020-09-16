5594fe914782adb491bcd4e23f3cffbbf3216479
Sarah Choung <sarahjchoung@gmail.com>
2020-09-15 18:12:58 -0700

    Added launch details and rocket details

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { useQuery, gql } from "@apollo/client";
 import { Link } from "react-router-dom";
-import classNames from "classnames";
 
 const LAUNCH_QUERY = gql`
   query LaunchQuery($flightNumber: Int!) {
@@ -27,9 +26,40 @@ function LaunchQuery(props) {
   });
   if (loading) return <h4>Loading...</h4>;
   if (error) console.log(error);
-  console.log(data);
 
-  return <h1>Launch</h1>;
+  const {
+    mission_name,
+    flight_number,
+    launch_year,
+    launch_success,
+    rocket: { rocket_id, rocket_name, rocket_type },
+  } = data.launch;
+
+  return (
+    <div>
+      <h1 className="display-4 my-3">
+        <span className="text-dark">Mission:</span>
+        {mission_name}
+      </h1>
+      <h4 className="mb-3">Launch Details</h4>
+      <ul className="list-group">
+        <li className="list-group-item">Flight Number: {flight_number}</li>
+        <li className="list-group-item">Launch Year: {launch_year}</li>
+        <li className="list-group-item">
+          Launch Successful:{" "}
+          <span className={launch_success ? "text-success" : "text-danger"}>
+            {launch_success ? "Yes" : "No"}
+          </span>
+        </li>
+      </ul>
+      <h4 className="my-3">Rocket Details</h4>
+      <ul className="list-group">
+        <li className="list-group-item">Rocket ID: {rocket_id}</li>
+        <li className="list-group-item">Rocket Name: {rocket_name}</li>
+        <li className="list-group-item">Rocket Type: {rocket_type}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default class Launch extends React.Component {
