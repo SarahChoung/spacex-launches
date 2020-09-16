8d808f3fb838ef367e4d47b44245ed4a7430e42e
Sarah Choung <sarahjchoung@gmail.com>
2020-09-15 18:14:48 -0700

    added back button to homepage

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -58,6 +58,10 @@ function LaunchQuery(props) {
         <li className="list-group-item">Rocket Name: {rocket_name}</li>
         <li className="list-group-item">Rocket Type: {rocket_type}</li>
       </ul>
+      <hr />
+      <Link to="/" className="btn btn-secondary">
+        Back
+      </Link>
     </div>
   );
 }
